2dc892303adbfeaf759485820fe4ca685be922f2
Luke Ghenco <lukeghenco@gmail.com>
2017-07-03 12:26:58 -0400

    custom validations for form_for

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,41 +3,15 @@ class RegistrationsController < ApplicationController
     @user = User.new 
   end 
 
-  def create 
-    errors = {}
-    if params[:user][:first_name].nil?
-      errors[:first_name] = "First name must not be blank"
-    end
-
-    if params[:user][:last_name].nil?
-      errors[:last_name] = "Last name must not be blank"
-    end
-
-    if params[:user][:username].nil?
-      errors[:username] = "Username must not be blank"
-    end
-
-    if params[:user][:email].nil?
-      errors[:email] = "Email must not be blank"
-    end
-
-    if params[:user][:password].nil?
-      errors[:password] = "Password must not be blank"
-    end
-
-    if errors.empty?
-      user = User.new(user_params)
-      if user.save 
-        flash[:notice] = "User was created successfully"
-        redirect_to root_path 
-      # else 
-      #   flash[:errors] = user.errors
-      #   render :new
-      end 
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:notice] = "User was created successfully"
+      redirect_to root_path 
     else 
-      flash[:errors] = errors 
-      render :new 
-    end
+      flash[:errors] = @user.errors
+      render :new
+    end 
   end
 
   private 
