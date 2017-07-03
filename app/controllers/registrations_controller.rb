d6786ca849a3d1e4a76954a248ef8f9339c4dac3
Luke Ghenco <lukeghenco@gmail.com>
2017-07-03 11:01:06 -0400

    add solution

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < ApplicationController 
+
+  def new 
+    @user = User.new 
+  end 
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to root_path 
+    else 
+      flash[:errors] = @user.errors
+      render :new 
+    end 
+  end
+
+  private 
+
+    def user_params 
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "is invalid" }
 end
